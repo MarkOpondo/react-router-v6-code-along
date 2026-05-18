fe01a6784e87908bac5fdcecc5c264e6d96462a3
Mark Opondo <markopondoi.t@gmail.com>
2026-05-18 19:42:47 +0300

    add navbar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
+import NavBar from "../components/NavBar"
+
 export default function About() {
     return(
         <>
             <header>
                 {/* NavBar */}
+                <NavBar/>
             </header>
             <main>
                 <h1>This is my about component</h1>
